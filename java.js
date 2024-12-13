fea371d4b7492ff6941308d432c596559612aa65
djimcru <djimcru0801@g.educaand.es>
2024-12-13 14:22:44 +0100

    examen

---
+++ java.js
@@ -0,0 +1,20 @@
+document.getElementById('btn2').addEventListener('click', function ocultarCuadrado(){
+    document.getElementById('cuadrado').innerHTML='';
+});
+
+function getRandomBotMessage () {
+    const messages = [
+        "Creo que no.",
+        "¡Totalmente!",
+        "Eso suena interesante.",
+        "No estoy seguro de entender, ¿puedes explicar más?",
+        "¡Qué genial!",
+        "Hmm, déjame pensar...",
+        "¡Por supuesto!",
+        "No sé, pero suena divertido.",
+        "¿Puedes decirme más?",
+        "Eso es un misterio para mí.",
+        "¡Me encanta hablar contigo!"]
+
+    return messages[Math.floor(Math.random() * messages.length)];
+}
